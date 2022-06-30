de882d9ec7800d248811877519b1b6f435f2296e
karbzBlaire <karbzblaire67@gmail.com>
2022-06-30 13:32:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,22 +2,4 @@
 
 
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
