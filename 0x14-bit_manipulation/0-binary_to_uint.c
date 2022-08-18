8562fd98e028a2f629119d9f9066fe03d7935ca8
karbzBlaire <karbzblaire67@gmail.com>
2022-08-18 07:37:28 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
 *@b:binary.
 *Return:unsignedint.
 */
-unsigned int binary_to_uint(constchar*b)
+unsigned int binary_to_uint(const char *b)
 {
 unsigned int ui;
 int len, base_two;
@@ -21,7 +21,7 @@ for (len = 0; b[len] != '\0'; len++)
 
 for (len--, base_two = 1; len >=0; len--, base_two *= 2)
 {
-if (b[len]!= '0' && b[len] != '1')
+if (b[len] != '0' && b[len] != '1')
 {
 return (0);
 }
